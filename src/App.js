88981a2b5f4f721f2b7c4cf1a900e0e71890d35f
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-08-05 20:06:35 +0300

    dz-1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Anton";
+  const imageUrl =
+    "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg";
+  const favoriteSite = {
+    name: "Google",
+    url: "https://www.google.com/",
+  };
+  const num1 = 5;
+  const num2 = 10;
+  const colors = ["Червоний", "Синій", "Зелений"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{name}</h1>
+        <p>Ласкаво просимо до нашого сайту</p>
+        <img src={imageUrl} alt="Bird" />
+        <a href={favoriteSite.url}>{favoriteSite.name}</a>
+        <p>Сума чисел: {num1 + num2}</p>
+        <ul>
+          {colors.map((color) => (
+            <li key={color}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
